5b37f2d768882ace3d0c94aa11303f53e60345cb
ThatMike <michael.maryanoff@gmail.com>
2020-05-27 12:09:01 -0500

    Links to main pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,29 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import QuestionCreate from './questions/QuestionCreate';
-import QuestionResponse from './questions/QuestionResponse';
-import QuestionResult from './questions/QuestionResult';
 
 import HomePage from './pages/HomePage';
 import LeaderBoard from './pages/LeaderBoard';
 import Login from './pages/Login';
 
+import Header from './Header';
+
 
 const App = () => {
     return (
-        <BrowserRouter>
-            <Route path="/" exact component={HomePage} />
-            <Route path="/questions/new" exact component={QuestionCreate} />
-            <Route path="/leaderboard" exact component={LeaderBoard} />
-            <Route path="/login" exact component={Login} />
-        </BrowserRouter>
+        <div className="ui container">
+            
+            <BrowserRouter>
+            <div>
+                <Header />
+                <Route path="/" exact component={HomePage} />
+                <Route path="/questions/new" exact component={QuestionCreate} />
+                <Route path="/leaderboard" exact component={LeaderBoard} />
+                <Route path="/login" exact component={Login} />
+            </div>
+            </BrowserRouter>
+            
+        </div>
         
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-    return <div>Header</div>;
+    return (
+        <div className="ui secondary pointing menu">
+            <Link to="/" className="item">
+                Home
+            </Link>
+            <Link to="/questions/new" className="item">
+                New Question
+            </Link>
+            <Link to="/leaderboard" className="item">
+                Leaderboard
+            </Link>
+            <div className="right menu">
+                <Link to="/" className="item">
+                    All Questions
+                </Link>
+            </div>
+        </div>
+    );
 }
 
 export default Header;
