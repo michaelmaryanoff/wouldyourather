ce5e0a3b6cbc6d963edb324c7435f9246679defd
michaelmaryanoff <michael.maryanoff@gmail.com>
2020-06-01 15:51:20 -0500

    Render list

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fetchQuestions } from '../../actions';
+import { fetchQuestions, fetchUsers } from '../../actions';
 import { connect } from 'react-redux';
 
 class HomePage extends React.Component {
@@ -7,10 +7,33 @@ class HomePage extends React.Component {
         this.props.fetchQuestions()
     }
 
+    renderUnansweredList() {
+        return this.props.questions.map(question => {
+            return (
+                <div className="item" key={question.id}>
+                    <div className="content">
+                        <div className="header">{`${question.author} asks:`}</div>
+                            <div >
+                                {question.optionOne.text}
+                                <br />OR
+                                <br />
+                                {question.optionTwo.text}
+                            </div>
+                    </div>
+                </div>
+            )
+        })
+    }
+
     render() {
-
-
-        return <div>HomePage</div>;
+        return (
+            <div>
+                <h1>Questions</h1>
+                <div className="ui celled list">
+                    {this.renderUnansweredList()}
+                </div>
+            </div>
+        )
     }
     
 }
@@ -18,8 +41,9 @@ class HomePage extends React.Component {
 const mapStateToProps = (state) => {
     // Sets our current user state
     return {
-        questions: Object.values(state.questions)
+        questions: Object.values(state.questions),
+        users: Object.values(state.users)
     }
 }
 
-export default connect(mapStateToProps, { fetchQuestions })(HomePage);
+export default connect(mapStateToProps, { fetchQuestions, fetchUsers })(HomePage);
