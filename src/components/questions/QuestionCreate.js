f6679b481a054fea736bc76de5cf95fa7fb5cb93
michaelmaryanoff <michael.maryanoff@gmail.com>
2020-06-03 10:59:06 -0500

    Handle form submission

--- src/components/questions/QuestionCreate.js
+++ src/components/questions/QuestionCreate.js
@@ -14,12 +14,25 @@ class QuestionCreate extends React.Component {
             
         )
     }
+
+    onSubmit(formValues) {
+        // FormValues is used instead of the event object because we no longer care about the event object
+        // We have all that stuff taken care of by Redux Form
+        console.log(formValues);
+        
+    }
+
     render() {
+        console.log(this.props);
+        
         return (
-            <form className="ui form">
-                <Field name="optionOne" component={this.renderInput} label="Option One" />
-                <Field name="optionTwo" component={this.renderInput} label="Option Two"/>
-            </form>
+            <div>
+                <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
+                    <Field name="optionOne" component={this.renderInput} label="Option One" />
+                    <Field name="optionTwo" component={this.renderInput} label="Option Two"/>
+                    <button className="ui button primary">Submit</button>
+                </form>
+            </div>
         )
     }
 }
