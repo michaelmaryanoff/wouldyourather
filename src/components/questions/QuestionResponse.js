0c0c0d6f0d7edfe572b7abc7ec96ea16ff7f4e38
michaelmaryanoff <michael.maryanoff@gmail.com>
2020-06-03 17:44:51 -0500

    Initial setup and pass value through field

--- src/components/questions/QuestionResponse.js
+++ src/components/questions/QuestionResponse.js
@@ -2,29 +2,54 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 class QuestionResponse extends React.Component {
-    renderInput({input, label}) {
-        return (
-            <div className="grouped fields">
-                <div className="field">
-                    <div className="ui radio checkbox">
-                        <label>{label}</label>
-                        <input type="radio" {...input} />
-                    </div>
-                </div>
-                
-            </div>
-        )
-    }
-  render() {
+  renderInput({ input, label }) {
+      console.log(input);
+      
     return (
-        <div className="ui form">
-            <Field name="optionOne" component={this.renderInput} label="First Option" />
-            <Field name="optionTwo" component={this.renderInput} label="Second option" />
+      <div className="grouped fields">
+        <div className="field">
+            <label>{label}</label>
+            <input type="radio" checked="checked" {...input} />
         </div>
-    )
+      </div>
+    );
+  }
+
+  onSubmit(formValues) {
+    console.log(formValues);
+  }
+
+  render() {
+      console.log(this.props);
+      
+    return (
+      <div>
+        <form
+          onSubmit={this.props.handleSubmit(this.onSubmit)}
+          className="ui form"
+        >
+        <label>Would you rather?</label>
+          <Field
+            name="optionOne"
+            component={this.renderInput}
+            type="radio"
+            label="First Option"
+            value="optionOne"
+          />
+          <Field
+            name="optionTwo"
+            component={this.renderInput}
+            type="radio"
+            label="Second option"
+            value="optionTwo"
+          />
+          <button className="ui button primary">Submit Answer</button>
+        </form>
+      </div>
+    );
   }
 }
 
 export default reduxForm({
-    form: 'questionResponse'
+  form: 'questionResponse'
 })(QuestionResponse);
