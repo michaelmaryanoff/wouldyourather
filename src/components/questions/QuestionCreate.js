84a3338b7b8918b6eae85f65cc229013fc1b9caa
michaelmaryanoff <michael.maryanoff@gmail.com>
2020-06-03 10:48:47 -0500

    Basic form setup

--- src/components/questions/QuestionCreate.js
+++ src/components/questions/QuestionCreate.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
-const QuestionCreate = () => {
-    return <div>QuestionCreate</div>;
+class QuestionCreate extends React.Component {
+    renderInput(formProps) {
+        // formProps is automatically created with the component attribute
+        // This can be destructured but we are not for now
+        // Label is being passed through as a prop to our form component
+        return (
+            <div className="field">
+                <label>{formProps.label}</label>
+                <input {...formProps.input}/>
+            </div>
+            
+        )
+    }
+    render() {
+        return (
+            <form className="ui form">
+                <Field name="optionOne" component={this.renderInput} label="Option One" />
+                <Field name="optionTwo" component={this.renderInput} label="Option Two"/>
+            </form>
+        )
+    }
 }
 
-export default QuestionCreate
+export default reduxForm({
+    // Redux form stores you info with this key in your store
+    form: 'questionCreate'
+})(QuestionCreate);
