1f9c18863d4f32b56385f59f0c57896256929b23
michaelmaryanoff <michael.maryanoff@gmail.com>
2020-06-05 16:17:48 -0500

    Reformat with prettier

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,61 +1,58 @@
-import { combineReducers } from 'redux';
-import { reducer as formReducer } from 'redux-form';
+import { combineReducers } from "redux";
+import { reducer as formReducer } from "redux-form";
 
-// TODO: make a reducer for list of users
+const currentUserReducer = (currentUser = null, action) => {
+  // Checks to see if we are using a sign in action
+  if (action.type === "SIGN_IN") {
+    console.log("currentUserReducer -> action.payload", action.payload);
+    return action.payload;
+  } else if (action.type === "SIGN_OUT") {
+    return null;
+  }
+  return currentUser;
+};
 
-const currentUserReducer = (currentUser=null, action) => {
+const fetchUsersReducer = (state = [], action) => {
+  if (action.type === "FETCH_USERS") {
+    return action.payload;
+  }
+  return state;
+};
 
-    // Checks to see if we are using a sign in action
-    if (action.type === 'SIGN_IN') {
-        return action.payload
-    } else if (action.type === 'SIGN_OUT') {
-        return null
-    }
-    return currentUser;
-}
+const fetchQuestionsReducer = (state = {}, action) => {
+  if (action.type === "FETCH_QUESTIONS") {
+    return action.payload;
+  }
+  return state;
+};
 
-const fetchUsersReducer = (state=[], action) => {
-    if (action.type === 'FETCH_USERS') {
-        return action.payload
-    }
-    return state
-}
+const addQuestionReducer = (state = {}, action) => {
+  if (action.type === "ADD_QUESTION") {
+    return action.payload;
+  }
+  return state;
+};
 
-const fetchQuestionsReducer = (state={}, action) => {
-    if (action.type === 'FETCH_QUESTIONS') {
-        return action.payload
-    }
-    return state
-}
+const getselectedQuestionReducer = (state = {}, action) => {
+  if (action.type === "SELECT_QUESTION") {
+    return action.payload;
+  }
+  return state;
+};
 
-const addQuestionReducer = (state={}, action) => {
-    if (action.type === 'ADD_QUESTION') {
-        return action.payload
-    }
-    return state
-}
-
-const getselectedQuestionReducer = (state={}, action) => {
-    if (action.type === 'SELECT_QUESTION') {
-        return action.payload
-    }
-    return state
-}
-
-const submitQuestionResponseReducer=(state={}, action) => {
-    if (action.type === 'SUBMIT_RESPONSE') {
-        return action.payload
-    }
-    return state
-}
-
+const submitQuestionResponseReducer = (state = {}, action) => {
+  if (action.type === "SUBMIT_RESPONSE") {
+    return action.payload;
+  }
+  return state;
+};
 
 export default combineReducers({
-    currentUser: currentUserReducer,
-    users: fetchUsersReducer,
-    questions: fetchQuestionsReducer,
-    form: formReducer,
-    addQuestion: addQuestionReducer,
-    selectedQuestion: getselectedQuestionReducer,
-    submittedQuestion: submitQuestionResponseReducer
+  currentUser: currentUserReducer,
+  users: fetchUsersReducer,
+  questions: fetchQuestionsReducer,
+  form: formReducer,
+  addQuestion: addQuestionReducer,
+  selectedQuestion: getselectedQuestionReducer,
+  submittedQuestion: submitQuestionResponseReducer
 });
