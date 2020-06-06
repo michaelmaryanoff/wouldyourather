e4fab89dd2c1628fced69fe4ef680784674879a1
michaelmaryanoff <michael.maryanoff@gmail.com>
2020-06-06 00:14:15 -0500

    Hook up state

--- src/components/pages/LeaderBoard.js
+++ src/components/pages/LeaderBoard.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from "react";
+import { fetchUsersAndQuestions } from "../../actions";
+import { connect } from "react-redux";
 
-const LeaderBoard = () => {
+class LeaderBoard extends React.Component {
+  componentDidMount() {
+    this.props.fetchUsersAndQuestions();
+  }
+  render() {
+    console.log("questions in render", this.props.questions);
+
     return <div>LeaderBoard</div>;
+  }
 }
 
-export default LeaderBoard;
+const mapStateToProps = state => {
+  return {
+    questions: Object.values(state.questions)
+  };
+};
+
+export default connect(mapStateToProps, { fetchUsersAndQuestions })(LeaderBoard);
