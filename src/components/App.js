33f48c7e104f1806fcb13ba3079a6c8d31dd20e1
michaelmaryanoff <michael.maryanoff@gmail.com>
2020-06-08 12:01:11 -0500

    Working redirect with Home

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,73 @@
-import React from 'react';
-import { Router, Route } from 'react-router-dom';
-import QuestionCreate from './questions/QuestionCreate';
-import QuestionResponse from './questions/QuestionResponse'
-import QuestionResult from './questions/QuestionResult'
-import history from '../history'
+import React from "react";
+import { Router, Route, Redirect } from "react-router-dom";
+import QuestionCreate from "./questions/QuestionCreate";
+import QuestionResponse from "./questions/QuestionResponse";
+import QuestionResult from "./questions/QuestionResult";
+import history from "../history";
+import _ from "lodash";
 
-import HomePage from './pages/HomePage';
-import LeaderBoard from './pages/LeaderBoard';
-import Login from './pages/Login';
+import HomePage from "./pages/HomePage";
+import LeaderBoard from "./pages/LeaderBoard";
+import Login from "./pages/Login";
 
-import Header from './Header';
+import Header from "./Header";
+import { connect } from "react-redux";
 
-const App = () => {
-    // TODO: Create a protected route to ensure you can't click on header when authenticated
+class App extends React.Component {
+  componentDidMount() {}
 
+  checkUser() {
+    let userBool = true;
+
+    if (_.isEmpty(this.props.currentUser)) {
+      console.log("is empty");
+
+      userBool = false;
+    } else {
+      console.log("not empty");
+
+      userBool = true;
+    }
+    console.log("user bool in function", userBool);
+
+    return userBool;
+  }
+
+  redirectUser() {}
+
+  render() {
     return (
-        <div className="ui container">
-            <Router history={history}>
-            <div>
-                <Header />
-                <Route path="/home" exact component={HomePage} />
-                <Route path="/questions/new" exact component={QuestionCreate} />
-                <Route path="/leaderboard" exact component={LeaderBoard} />
-                <Route path="/" exact component={Login} />
-                <Route path="/questions/response/:id" exact component={QuestionResponse} />
-                <Route path="/questions/result/:id" exact component={QuestionResult} />
-            </div>
-            </Router>
-            
-        </div>
-        
-    )
+      <div className="ui container">
+        <Router history={history}>
+          <div>
+            <Header />
+            <Route path="/home" exact component={HomePage}>
+              {_.isEmpty(this.props.currentUser) ? (
+                <Redirect to="/" />
+              ) : (
+                <HomePage />
+              )}
+            </Route>
+            <Route path="/questions/new" exact component={QuestionCreate}></Route>
+            <Route path="/leaderboard" exact component={LeaderBoard} />
+            <Route path="/" exact component={Login} />
+            <Route
+              path="/questions/response/:id"
+              exact
+              component={QuestionResponse}
+            />
+            <Route path="/questions/result/:id" exact component={QuestionResult} />
+          </div>
+        </Router>
+      </div>
+    );
+  }
 }
+const mapStateToProps = state => {
+  // Sets our current user state
+  return {
+    currentUser: state.currentUser
+  };
+};
 
-export default App;
+export default connect(mapStateToProps, {})(App);
