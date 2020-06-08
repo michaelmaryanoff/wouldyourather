b0128dfaa92b247ee51f3307afc9e4ab57610d26
michaelmaryanoff <michael.maryanoff@gmail.com>
2020-06-08 14:07:05 -0500

    Render dunny component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Logout from "../components/auth/Logout";
+import { connect } from "react-redux";
 
-const Header = () => {
-  return (
-    <div className="ui secondary pointing menu">
-      <Link to="/home" className="item">
-        Home
-      </Link>
-      <Link to="/questions/new" className="item">
-        New Question
-      </Link>
-      <Link to="/leaderboard" className="item">
-        Leaderboard
-      </Link>
-      <div className="right menu">
-        <Logout />
+class Header extends React.Component {
+  renderCurrentuser() {
+    return <div>Current user</div>;
+  }
+
+  render() {
+    return (
+      <div className="ui secondary pointing menu">
+        <Link to="/home" className="item">
+          Home
+        </Link>
+        <Link to="/questions/new" className="item">
+          New Question
+        </Link>
+        <Link to="/leaderboard" className="item">
+          Leaderboard
+        </Link>
+        <div className="right menu">
+          <div className="item">{this.renderCurrentuser()}</div>
+          <Logout />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    currentUser: state.currentUser
+  };
 };
 
-export default Header;
+export default connect(mapStateToProps)(Header);
