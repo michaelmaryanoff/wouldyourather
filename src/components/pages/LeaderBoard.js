d2107e18d7364805cf368a42cedabc3bb1c2e867
michaelmaryanoff <michael.maryanoff@gmail.com>
2020-06-08 11:19:40 -0500

    Populate UI

--- src/components/pages/LeaderBoard.js
+++ src/components/pages/LeaderBoard.js
@@ -1,21 +1,46 @@
 import React from "react";
 import { fetchUsersAndQuestions } from "../../actions";
 import { connect } from "react-redux";
+import _ from "lodash";
 
 class LeaderBoard extends React.Component {
   componentDidMount() {
     this.props.fetchUsersAndQuestions();
   }
-  render() {
-    console.log("questions in render", this.props.questions);
 
-    return <div>LeaderBoard</div>;
+  renderLeaderList() {
+    return this.props.users.map(user => {
+      const { questions, answers, name, id, avatarURL } = user;
+      const totalScore = user.questions.length + _.size(user.answers);
+      const avatarURLFull = require(`../../api${avatarURL}`);
+
+      return (
+        <div className="item" key={id}>
+          <div className="right floated content">Total score = {totalScore}</div>
+          <div className="content">
+            <img className="ui avatar image" alt="temp" src={avatarURLFull} />
+            <div className="header">{name}</div>
+            <div className="description">
+              Answered Questions = {_.size(answers)}
+              <br />
+              Created questions = {questions.length}
+              <br />
+            </div>
+          </div>
+        </div>
+      );
+    });
+  }
+
+  render() {
+    return <div className="ui celled list">{this.renderLeaderList()}</div>;
   }
 }
 
 const mapStateToProps = state => {
   return {
-    questions: Object.values(state.questions)
+    questions: Object.values(state.questions),
+    users: Object.values(state.users)
   };
 };
 
