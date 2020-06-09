914c32eac8da48dccf071f391025374ddab10390
michaelmaryanoff <michael.maryanoff@gmail.com>
2020-06-09 09:51:16 -0500

    Add back lodash

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
+import _ from 'lodash';
 import userReducer from './userReducer';
 
 const fetchQuestionsReducer = (state = {}, action) => {
