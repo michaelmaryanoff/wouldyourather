3f82cabab91c572a5d31364d95c1d7b14925583e
michaelmaryanoff <michael.maryanoff@gmail.com>
2020-06-12 09:46:08 -0500

    Add 404

--- src/components/auth/ErrorPage.js
+++ src/components/auth/ErrorPage.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import NotFound from '../../api/NotFound.jpg';
 
 class ErrorPage extends React.Component {
   render() {
-    return <div>There was an error!</div>;
+    return (
+      <div>
+        <img src={NotFound} alt="Page not found" />
+      </div>
+    );
   }
 }
 
